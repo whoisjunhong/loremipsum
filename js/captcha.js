36563267d8f71ba2eee47d10afe470aa9b0466c0
SeanHo <seanhowenbin@gmail.com>
2022-01-08 06:32:59 +0800

    midnight oil, did up captcha and end screen

--- js/captcha.js
+++ js/captcha.js
@@ -3,6 +3,10 @@ var clicks = 0
 
 function click() {
     clicks += 1;
-    var phrases = ["", "I am not a robot", "Yes I am", "True... how can I prove that I am not a robot?"]
+    var phrases = ["", "I am not a robot", "Yes I am", "True... how can I prove that I am not a robot?", "...", "Can we get this over and done with already?", "", "", "", "", "", "", "", "", "", "", "", ""]
+    console.log(clicks)
+    if(clicks === 6){
+        return(null)
+    }
     return(phrases[clicks])
 }
--- js/captcha_head.js
+++ js/captcha_head.js
@@ -3,6 +3,6 @@ var clicke = 0
 
 function click2() {
     clicke += 1;
-    var phrases = ["", "Please check the box below to proceed", "Are you sure?", "Isnt that what a robot would say?"]
+    var phrases = ["", "Please check the box below to proceed", "Are you sure?", "Isnt that what a robot would say?", "¯\\_(ツ)_/¯", '...', "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
     return(phrases[clicks])
 }
